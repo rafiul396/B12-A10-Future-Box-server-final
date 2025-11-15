9cd596c39b7595aaef0db600ee7c166926a63bec
rafiul from office <rafiulofficial396@gmail.com>
2025-11-15 17:31:00 +0600

    feat(server): add DELETE route to delete records from the collection

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/my-transaction/:id', async (req, res) => {
+            const { id } = req.params;
+            const filter = { _id: new ObjectId(id) };
+            const result = await dataCollection.deleteOne(filter)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
